cf2c03adafbbbbd7a9f9f1c146fcfd941aa564de
Khanhnd <khanhnguyen@gmail.com>
2024-08-01 17:00:19 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslocoModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
